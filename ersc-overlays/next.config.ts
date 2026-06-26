37d1e5edaff83b1142fba1e241ec2f862f94ad59
Charlie Anthony <charliejohnanthony@gmail.com>
2026-06-26 23:29:47 +0100

    updated asset prefix for production

--- ersc-overlays/next.config.ts
+++ ersc-overlays/next.config.ts
@@ -2,14 +2,14 @@ import type { NextConfig } from "next";
 import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
 
 const env = process.env.NODE_ENV;
-// assetPrefix: env == "production" ? "https://rsc.canth.dev/" : undefined,
+// assetPrefix: env == "production" ? "~/WebstormProjects/ERSC-Livestream-React/ersc-overlays/out" : undefined,
 
 const nextConfig: NextConfig = {
 	output: "export",
 	images: {
 		unoptimized: true,
 	},
-	assetPrefix: env == "production" ? "~/WebstormProjects/ERSC-Livestream-React/ersc-overlays/out" : undefined,
+	assetPrefix: env == "production" ? "https://rsc.canth.dev/" : undefined,
 };
 
 const withVanillaExtract = createVanillaExtractPlugin();
