4e8d8eade63a2b8cff3db57bfe6e43c98af4f689
Charlie Anthony <charliejohnanthony@gmail.com>
2026-07-09 20:38:38 +0100

    created results screen

---
+++ ersc-overlays/app/rscast/results/page.css.ts
@@ -0,0 +1,122 @@
+import { style } from "@vanilla-extract/css";
+
+export const logo = style({
+	position: "fixed",
+	left: "85px",
+	top: "1005px",
+});
+
+export const sponsors = style({
+	position: "fixed",
+	left: "1290px",
+	top: "1003px",
+});
+
+export const conferences = style({
+	position: "fixed",
+	left: "0px",
+	top: "0px",
+	width: "1920px",
+	height: "1080px",
+});
+
+export const tier = style({
+	position: "fixed",
+	left: "920px",
+	top: "23px",
+});
+
+export const rscLogo = style({
+	position: "fixed",
+	left: "856px",
+	top: "436px",
+});
+
+export const result = style({
+	position: "fixed",
+	top: "var(--dynamic-top)",
+	left: "var(--dynamic-left)",
+	width: "781px",
+	height: "174px",
+});
+
+export const title = style({
+	position: "fixed",
+	top: "100px",
+	left: 0,
+	width: "1920px",
+	height: "40px",
+	fontSize: "36px",
+	color: "white",
+	textAlign: "center",
+	fontFamily: "Anta",
+	zIndex: 2,
+});
+
+export const logoLeft = style({
+	position: "absolute",
+	top: "77px",
+	left: "20px",
+	width: "70px",
+	height: "65px",
+	maskImage: "url('/rscast/schedule/maskLeft.png')",
+	maskSize: "contain",
+	maskRepeat: "no-repeat",
+});
+
+export const logoRight = style({
+	position: "absolute",
+	top: "77px",
+	right: "20px",
+	width: "70px",
+	height: "65px",
+	maskImage: "url('/rscast/schedule/maskRight.png')",
+	maskSize: "contain",
+	maskRepeat: "no-repeat",
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center",
+});
+
+export const leftTeamName = style({
+	position: "absolute",
+	top: "90px",
+	left: "100px",
+	width: "225px",
+	height: "40px",
+	fontSize: "26px",
+	color: "white",
+	textAlign: "center",
+	fontFamily: "Anta",
+	zIndex: 2,
+	textWrap: "nowrap",
+	overflow: "hidden",
+});
+
+export const rightTeamName = style({
+	position: "absolute",
+	top: "90px",
+	right: "100px",
+	width: "225px",
+	height: "40px",
+	fontSize: "26px",
+	color: "white",
+	textAlign: "center",
+	fontFamily: "Anta",
+	zIndex: 2,
+	textWrap: "nowrap",
+	overflow: "hidden",
+});
+
+export const score = style({
+	position: "absolute",
+	top: "90px",
+	left: "325px",
+	width: "130px",
+	height: "40px",
+	fontSize: "30px",
+	color: "white",
+	textAlign: "center",
+	fontFamily: "Anta",
+	zIndex: 2,
+});
---
+++ ersc-overlays/app/rscast/results/page.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import * as styles from "./page.css";
+import { SHEETS_CONFIG } from "@/lib/sheets";
+import { getCell } from "@/lib/callHelpers";
+import Image from "next/image";
+import { useGoogleSheet } from "@/hooks/useGoogleSheet";
+import { useState, useEffect } from "react";
+
+enum backgrounds {
+	GLACES,
+	IGNIS,
+	CROSSCONFERENCE
+}
+
+export default function CasterOverlay() {
+	const sheet = useGoogleSheet(SHEETS_CONFIG);
+	const [isCrossConference, setIsCrossConference] = useState<boolean>(false);
+
+	useEffect(() => {
+		if (sheet) {
+			// eslint-disable-next-line react-hooks/set-state-in-effect
+			setIsCrossConference(getCell(sheet, "M3") == "TRUE");
+		}
+	}, [sheet]);
+
+	if (!sheet) {
+		return;
+	}
+
+	return (
+		<>
+			{!isCrossConference &&
+				<Image src="/rscast/schedule/Conferences.png" alt="Conference Titles" className={styles.conferences}
+					   width={1920} height={1080} />}
+			<Image src="/rscast/RSCastLogo.png" alt="RSCast Logo" className={styles.logo} width={200} height={53} />
+			<Image src="/rscast/Sponsors.png" alt="RSCast Logo" className={styles.sponsors} width={558} height={61} />
+			<Image src={getCell(sheet, "M2")} alt="Tier Logo" className={styles.tier} width={80} height={92} />
+			<Image src={"https://images.rsc-community.com/EU/Logo.png"} alt="RSC Logo" className={styles.rscLogo}
+				   width={206} height={206} />
+			<span className={styles.title}>{getCell(sheet, "N2").toUpperCase()}</span>
+
+			{Array.from({ length: 10 }, (_, i) => (
+				<Result key={"left" + i} index={i} type={isCrossConference ? backgrounds.CROSSCONFERENCE : backgrounds.GLACES} sheet={sheet} left />
+			))}
+
+			{Array.from({ length: 10 }, (_, i) => (
+				<Result key={"left" + i} index={i} type={isCrossConference ? backgrounds.CROSSCONFERENCE : backgrounds.IGNIS} sheet={sheet} />
+			))}
+
+		</>
+	);
+}
+
+type ResultProps = {
+	index: number;
+	type: backgrounds;
+	left?: boolean;
+	sheet: string[][];
+};
+
+const Result: React.FC<ResultProps> = ({ index, type, left, sheet }) => {
+	let back = "";
+
+	switch (type) {
+		case backgrounds.CROSSCONFERENCE:
+			back = "/rscast/schedule/crossConf.png";
+			break;
+		case backgrounds.GLACES:
+			back = "/rscast/schedule/glacies.png";
+			break;
+		case backgrounds.IGNIS:
+			back = "/rscast/schedule/ignis.png";
+			break;
+	}
+
+	const row: string = (left ? 10 + (2 + index) : (2 + index)).toString();
+
+	return (
+		<div className={styles.result} style={
+			{
+				"--dynamic-top": `${100 + (index * 82)}px`,
+				"--dynamic-left": `${!!left ? "50" : "1095"}px`,
+			} as React.CSSProperties
+		}>
+			<Image
+				src={back}
+				alt="Result background"
+				width={781}
+				height={174}
+
+			/>
+			<div className={styles.logoLeft}>
+				<Image
+					src={getCell(sheet, "K" + row)}
+					alt="Team Logo"
+					width={120}
+					height={120}
+					style={{
+						objectFit: "cover",
+						objectPosition: "center",
+						transform: "scale(1.4)",
+					}}
+				/>
+			</div>
+			<div className={styles.logoRight}>
+				<Image
+					src={getCell(sheet, "L" + row)}
+					alt="Team Logo"
+					width={120}
+					height={120}
+					style={{
+						objectFit: "cover",
+						objectPosition: "center",
+						transform: "scale(1.4)",
+					}}
+				/>
+			</div>
+			<span className={styles.leftTeamName}>{getCell(sheet, "H" + row)}</span>
+			<span className={styles.rightTeamName}>{getCell(sheet, "J" + row)}</span>
+			<span className={styles.score}>{getCell(sheet, "I" + row)}</span>
+		</div>
+	);
+};
+
